5412b524fb6b929ae8d076d726482c298782e63c
P <30462819+ptrpfa@users.noreply.github.com>
2021-12-18 18:48:29 +0800

    [2] Pending calculations

--- analyse.py
+++ analyse.py
@@ -1,14 +1,11 @@
-import os
+import os, re, json, datetime
 import pandas as pd
-import re
 
 # Constants
 file_dir = 'history'
 regex_trade = '^trade_history.+\.csv'
 regex_wallet = '^wallet_history.+\.csv'
-
-# To set column names
-trade_cols = ['Pair', 'Side', 'Type', 'Average Price', 'Price', 'Amount', 'Executed', 'Fee', 'Total', 'Status', 'Time & Date']
+overall_wallet = {'SGD':0}
 
 # Entrypoint
 list_dir = os.listdir(file_dir)
@@ -21,6 +18,34 @@ for f in list_dir:
         file_wallet = f
 
 df_trade = pd.read_csv("%s/%s" % (file_dir,file_trade))
-df_wallet = pd.read_csv("%s/%s" % (file_dir,file_wallet))
+df_trade = df_trade[df_trade['Status']=='Completed']
+df_trade.drop(columns=['Type', 'Average Price', 'Executed', 'Status'], inplace=True)
+df_trade.sort_values(by="Time & Date", inplace=True)
 
-# To drop unusable columns
+df_wallet = pd.read_csv("%s/%s" % (file_dir,file_wallet))
+df_wallet = df_wallet[df_wallet['Status (All)']=='Completed']
+df_wallet.drop(columns=['Transaction Hash', 'To Address', 'Received by Address', 'Fee', 'Note', 'Status (All)'], inplace=True)
+df_wallet.sort_values(by="Date & Time (*-*)", inplace=True)
+
+js_trade = json.loads(df_trade.to_json(orient='records'))
+js_wallet = json.loads(df_wallet.to_json(orient='records'))
+
+# Iterate wallet transactions (To figure out)
+for item in js_wallet:
+    if (item['Currency(All)']=='SGD'):
+        if (item['Type (All)']=='Fiat Deposit' or item['Type (All)']=='Referral Commission'):
+            # Add to wallet
+            overall_wallet['SGD']+=item['Amount']
+            print ("Addition:")
+            print (item)
+            print ("Wallet: ", overall_wallet['SGD'])
+        elif (item['Type (All)']=='Fiat Withdrawal'):
+            # Remove from wallet
+            # overall_wallet['SGD']-=item['Amount']
+            # print ("Removal:")
+            # print (item)
+            # print ("Wallet: ", overall_wallet['SGD'])
+            pass
+    else:
+        pass
+
